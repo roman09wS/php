408a134e4859faa5c7f1b71c22d5ec47db33a438
roman09wS <winderroman3131@gmail.com>
2023-09-12 11:02:42 -0500

    empezando crud de user con login

---
+++ usuarios/application/db/datos.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-09-2023 a las 14:17:44
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `datos`
+--
+CREATE DATABASE IF NOT EXISTS `datos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `datos`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `persona_id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `estado_civil` varchar(100) DEFAULT NULL,
+  `php` varchar(100) DEFAULT 'no sabe',
+  `html` varchar(100) DEFAULT 'no sabe',
+  `python` varchar(100) DEFAULT 'no sabe',
+  `aws` varchar(100) DEFAULT 'no sabe'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`persona_id`, `nombre`, `apellido`, `edad`, `genero`, `estado_civil`, `php`, `html`, `python`, `aws`) VALUES
+(25, 'Mauricio', 'Roman', 38, 'Masculino', 'soltero', 'si sabe', 'si sabe', NULL, NULL),
+(26, 'Mauricio', 'Roman', 37, 'Masculino', 'casado', 'si sabe', NULL, NULL, NULL),
+(27, 'Mauricio', 'Roman', 100, 'Masculino', 'soltero', 'si sabe', 'si sabe', 'no sabe', NULL),
+(28, 'Mauricio', 'Roman', 24, 'Masculino', 'viudo', 'si sabe', 'no sabe', NULL, 'si sabe'),
+(32, 'Carlos', 'Bustamaneta', 29, 'Masculino', 'viudo', 'si sabe', 'si sabe', 'si sabe', 'si sabe'),
+(34, 'Carlos', 'Bustamante', 9999, 'Masculino', 'soltero', 'si sabe', 'si sabe', 'si sabe', 'si sabe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `correo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`persona_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `persona_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
